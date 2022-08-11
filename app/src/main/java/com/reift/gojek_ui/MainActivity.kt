f7eb57d9b7d81c32f1a836938701f5f233ade1c2
ReihanFatilla <reihanfatilla@gmail.com>
2022-08-11 10:17:13 +0700

    [feat]: setted up starter activity

--- app/src/main/java/com/reift/gojek_ui/MainActivity.kt
+++ app/src/main/java/com/reift/gojek_ui/MainActivity.kt
@@ -2,12 +2,22 @@ package com.reift.gojek_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.reift.gojek_ui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding as ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpViewPager()
+
+    }
+
+    private fun setUpViewPager() {
     }
 }
